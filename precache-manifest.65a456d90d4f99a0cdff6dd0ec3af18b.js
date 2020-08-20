64b62197ca690d71784acac3205a2b55cd467360
Renish Charaniya <renish9923@gmail.com>
2020-08-21 01:04:01 +0530

    Updates

---
+++ precache-manifest.65a456d90d4f99a0cdff6dd0ec3af18b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a214d74c39e347db16a5ec6264cb065d",
+    "url": "/codestagram/index.html"
+  },
+  {
+    "revision": "127f8e670ed38fddc1a3",
+    "url": "/codestagram/static/css/main.0d2dae9c.chunk.css"
+  },
+  {
+    "revision": "0ca96c1ddd98a8367201",
+    "url": "/codestagram/static/js/2.3409ea70.chunk.js"
+  },
+  {
+    "revision": "6c8202bc20827f0979ac9cc13f2fae8d",
+    "url": "/codestagram/static/js/2.3409ea70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "127f8e670ed38fddc1a3",
+    "url": "/codestagram/static/js/main.299b1a6d.chunk.js"
+  },
+  {
+    "revision": "7e0643c5717a825118a2",
+    "url": "/codestagram/static/js/runtime-main.959c7930.js"
+  }
+]);
